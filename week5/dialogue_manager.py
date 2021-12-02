050753357daca0630d04f619ddf5abdcf22a160d
sd704 <51424617+sd704@users.noreply.github.com>
2021-12-02 17:34:04 +0530

    Update dialogue_manager.py

--- week5/dialogue_manager.py
+++ week5/dialogue_manager.py
@@ -2,7 +2,6 @@ import os
 from sklearn.metrics.pairwise import pairwise_distances_argmin
 
 from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
 from utils import *
 
 
@@ -24,8 +23,8 @@ class ThreadRanker(object):
 
         # HINT: you have already implemented a similar routine in the 3rd assignment.
         
-        question_vec = #### YOUR CODE HERE ####
-        best_thread = #### YOUR CODE HERE ####
+        question_vec = question_to_vec(question, self.word_embeddings, self.embeddings_dim).reshape(1,-1) 
+        best_thread = pairwise_distances_argmin(question_vec, thread_embeddings) 
         
         return thread_ids[best_thread]
 
@@ -43,27 +42,15 @@ class DialogueManager(object):
         # Goal-oriented part:
         self.tag_classifier = unpickle_file(paths['TAG_CLASSIFIER'])
         self.thread_ranker = ThreadRanker(paths)
-        self.__init_chitchat_bot()
 
-    def __init_chitchat_bot(self):
+        self.create_chitchat_bot() # added to Initialize the bot
+
+    def create_chitchat_bot(self):
         """Initializes self.chitchat_bot with some conversational model."""
-
-        # Hint: you might want to create and train chatterbot.ChatBot here.
-        # Create an instance of the ChatBot class.
-        # Set a trainer set_trainer(ChatterBotCorpusTrainer) for the ChatBot.
-        # Train the ChatBot with "chatterbot.corpus.english" param.
-        # Note that we use chatterbot==0.7.6 in this project. 
-        # You are welcome to experiment with other versions but they might have slightly different API.
         
-        ########################
-        #### YOUR CODE HERE ####
-        ########################
+        self.chitchat_bot = ChatBot('Sonny', trainer='chatterbot.trainers.ChatterBotCorpusTrainer')
+        self.chitchat_bot.train("chatterbot.corpus.english")
 
-        # remove this when you're done
-        raise NotImplementedError(
-            "Open dialogue_manager.py and fill with your code. In case of Google Colab, download"
-            "(https://github.com/hse-aml/natural-language-processing/blob/master/project/dialogue_manager.py), "
-            "edit locally and upload using '> arrow on the left edge' -> Files -> UPLOAD")
        
     def generate_answer(self, question):
         """Combines stackoverflow and chitchat parts using intent recognition."""
@@ -71,22 +58,22 @@ class DialogueManager(object):
         # Recognize intent of the question using `intent_recognizer`.
         # Don't forget to prepare question and calculate features for the question.
         
-        prepared_question = #### YOUR CODE HERE ####
-        features = #### YOUR CODE HERE ####
-        intent = #### YOUR CODE HERE ####
+        prepared_question = text_prepare(question) 
+        features = self.tfidf_vectorizer.transform([prepared_question]) 
+        intent = self.intent_recognizer.predict(features)[0]
 
         # Chit-chat part:   
         if intent == 'dialogue':
             # Pass question to chitchat_bot to generate a response.       
-            response = #### YOUR CODE HERE ####
+            response = self.chitchat_bot.get_response(question) 
             return response
         
         # Goal-oriented part:
         else:        
             # Pass features to tag_classifier to get predictions.
-            tag = #### YOUR CODE HERE ####
+            tag = self.tag_classifier.predict(features)[0] 
             
             # Pass prepared_question to thread_ranker to get predictions.
-            thread_id = #### YOUR CODE HERE ####
-            
+            thread_id = self.thread_ranker.get_best_thread(question, tag)[0] 
+           
             return self.ANSWER_TEMPLATE % (tag, thread_id)
