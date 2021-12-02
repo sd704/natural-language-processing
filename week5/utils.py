8b0249afe98e7943554b68d9d321c4d6dc865fa5
sd704 <51424617+sd704@users.noreply.github.com>
2021-12-02 17:30:39 +0530

    Update utils.py

--- week5/utils.py
+++ week5/utils.py
@@ -12,19 +12,20 @@ RESOURCE_PATH = {
     'TAG_CLASSIFIER': 'tag_classifier.pkl',
     'TFIDF_VECTORIZER': 'tfidf_vectorizer.pkl',
     'THREAD_EMBEDDINGS_FOLDER': 'thread_embeddings_by_tags',
-    'WORD_EMBEDDINGS': 'data/word_embeddings.tsv',
+    'WORD_EMBEDDINGS': 'word_embeddings.tsv',
 }
 
 
 def text_prepare(text):
     """Performs tokenization and simple preprocessing."""
+    
     replace_by_space_re = re.compile('[/(){}\[\]\|@,;]')
-    good_symbols_re = re.compile('[^0-9a-z #+_]')
+    bad_symbols_re = re.compile('[^0-9a-z #+_]')
     stopwords_set = set(stopwords.words('english'))
 
     text = text.lower()
     text = replace_by_space_re.sub(' ', text)
-    text = good_symbols_re.sub('', text)
+    text = bad_symbols_re.sub('', text)
     text = ' '.join([x for x in text.split() if x and x not in stopwords_set])
 
     return text.strip()
@@ -38,37 +39,38 @@ def load_embeddings(embeddings_path):
       embeddings - dict mapping words to vectors;
       embeddings_dim - dimension of the vectors.
     """
-
+    
     # Hint: you have already implemented a similar routine in the 3rd assignment.
     # Note that here you also need to know the dimension of the loaded embeddings.
     # When you load the embeddings, use numpy.float32 type as dtype
 
-    ########################
-    #### YOUR CODE HERE ####
-    ########################
+    embeddings = {}
 
-    # remove this when you're done
-    raise NotImplementedError(
-        "Open utils.py and fill with your code. In case of Google Colab, download"
-        "(https://github.com/hse-aml/natural-language-processing/blob/master/project/utils.py), "
-        "edit locally and upload using '> arrow on the left edge' -> Files -> UPLOAD")
+    with open(embeddings_path, encoding='utf-8') as f:
+        for ln in f.readlines():
+            rw = ln.strip().split('\t')
+            embeddings[rw[0]] = np.array(rw[1:], dtype=np.float32)
+    embeddings_dim = embeddings[list(embeddings)[0]].shape[0]
+    
+    return embeddings, embeddings_dim
 
 
 def question_to_vec(question, embeddings, dim):
     """Transforms a string to an embedding by averaging word embeddings."""
-
+    
     # Hint: you have already implemented exactly this function in the 3rd assignment.
 
-    ########################
-    #### YOUR CODE HERE ####
-    ########################
-
-    # remove this when you're done
-    raise NotImplementedError(
-        "Open utils.py and fill with your code. In case of Google Colab, download"
-        "(https://github.com/hse-aml/natural-language-processing/blob/master/project/utils.py), "
-        "edit locally and upload using '> arrow on the left edge' -> Files -> UPLOAD")
-
+    vec = np.zeros((dim,), dtype=np.float32)
+    count = 0
+    for word in question.split():
+      if word in embeddings:
+        vec += embeddings[word]
+        count += 1
+           
+    if count == 0:
+        return vec
+      
+    return vec/count
 
 def unpickle_file(filename):
     """Returns the result of unpickling the file content."""
